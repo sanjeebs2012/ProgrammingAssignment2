6cf1cb12d765a902b0093a1584809f76ad83fe10
sanjeebs2012 <67054643+sanjeebs2012@users.noreply.github.com>
2022-01-14 15:34:29 +0900

    Create Assignment.R

---
+++ Assignment.R
@@ -0,0 +1,34 @@
+#Caching the Inverse of a Matrix
+#makeCacheMatrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+#cacheSolve
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+data<-matrix(c(10,9,8,7),2,2)
+datacache<-makeCacheMatrix(data)
+cacheSolve(datacache)
